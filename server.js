8c34380b03aa6ba58a03064533e76e3dae896075
Arno van Urk <avanurk@appliedmedical.com>
2025-07-04 12:41:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const mime = require('mime');
+const mime = require('mime-types');
 const fs = require('fs');
 const path = require('path');
 const app = express();
